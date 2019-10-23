c27c2a3b41eaa0a05e3508a564ae2518e7cd2c62
Yann Mancel <yann.mancel@hotmail.fr>
2019-10-23 16:02:08 +0200

    Update the speed dial of FABs
    Update file:
    DiceActivity (Kotlin class, update the speed dial of FABs)
    layout_fabs (XML file, add 2 Mini FABs)

--- app/src/main/java/com/mancel/yann/randomgame/views/activities/DiceActivity.kt
+++ app/src/main/java/com/mancel/yann/randomgame/views/activities/DiceActivity.kt
@@ -1,6 +1,9 @@
 package com.mancel.yann.randomgame.views.activities
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.graphics.drawable.Animatable
+import android.support.constraint.ConstraintLayout
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -23,17 +26,25 @@ import kotlinx.android.synthetic.main.layout_fabs.*
  *
  * A [BaseActivity] subclass which implements [View.OnClickListener].
  */
+
+const val TRANSLATION_Y = "translationY"
+
 class DiceActivity : BaseActivity(), View.OnClickListener {
 
     // FIELDS --------------------------------------------------------------------------------------
 
+    private lateinit var mLayoutContainer: ConstraintLayout
     private lateinit var mFab0: FloatingActionButton
     private lateinit var mFab1: FloatingActionButton
+    private lateinit var mFab2: FloatingActionButton
+    private lateinit var mFab3: FloatingActionButton
     private lateinit var mRecyclerView: RecyclerView
 
     private lateinit var mAdapter: DiceAdapter
     private var isExpanded = false
     private var mOffset1: Float = 0F
+    private var mOffset2: Float = 0F
+    private var mOffset3: Float = 0F
 
     // METHODS -------------------------------------------------------------------------------------
 
@@ -44,8 +55,11 @@ class DiceActivity : BaseActivity(), View.OnClickListener {
     override fun getToolBar(): Toolbar = this.mToolbar
 
     override fun configureFields() {
+        this.mLayoutContainer = this.layout_fabs_constraint_layout
         this.mFab0 = this.layout_fabs_fab_0
         this.mFab1 = this.layout_fabs_fab_1
+        this.mFab2 = this.layout_fabs_fab_2
+        this.mFab3 = this.layout_fabs_fab_3
         this.mRecyclerView = this.activity_dice_RecyclerView
     }
 
@@ -69,6 +83,12 @@ class DiceActivity : BaseActivity(), View.OnClickListener {
 
                 // FAB 1: Displaying in Expandable mode
                 R.id.layout_fabs_fab_1 -> Toast.makeText(this, "FAB 1", Toast.LENGTH_SHORT).show()
+
+                // FAB 2: Displaying in Expandable mode
+                R.id.layout_fabs_fab_2 -> Toast.makeText(this, "FAB 2", Toast.LENGTH_SHORT).show()
+
+                // FAB 3: Displaying in Expandable mode
+                R.id.layout_fabs_fab_3 -> Toast.makeText(this, "FAB 3", Toast.LENGTH_SHORT).show()
 
                 // Error
                 else -> throw Exception("Impossible action: None of buttons is selected.")
@@ -95,29 +115,42 @@ class DiceActivity : BaseActivity(), View.OnClickListener {
 
     private fun configureListenerOfFABs() {
         // FAB 0
-        this.layout_fabs_fab_0.setOnClickListener(this)
+        this.mFab0.setOnClickListener(this)
 
         // FAB 1
-        this.layout_fabs_fab_1.setOnClickListener(this)
-        this.layout_fabs_fab_1.hide()
+        this.mFab1.setOnClickListener(this)
+        // this.mFab1.hide()
 
-//        this.layout_fabs_constraint_layout.viewTreeObserver.addOnPreDrawListener {
-//            this.layout_fabs_constraint_layout.viewTreeObserver.removeOnPreDrawListener(this);
-////            this.layout_fabs_constraint_layout.viewTreeObserver.removeOnPreDrawListener { this }
-//            this.mOffset1 = this.layout_fabs_fab_0.y - this.layout_fabs_fab_1.y
-//            this.layout_fabs_fab_1.translationY = this.mOffset1
-//
-//            true
-//        }
+        // FAB 2
+        this.mFab2.setOnClickListener(this)
+        // this.mFab2.hide()
 
-//        fabContainer.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-//            @Override
-//            public boolean onPreDraw() {
-//                fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
-//                offset1 = fab.getY() - fabAction1.getY();
-//                fabAction1.setTranslationY(offset1);
-//                return true;
-//            }
+        // FAB 3
+        this.mFab3.setOnClickListener(this)
+        // this.mFab3.hide()
+
+        // CONSTRAINT LAYOUT
+        this.mLayoutContainer.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // FAB 1
+                    mOffset1 = mFab0.y - mFab1.y
+                    mFab1.translationY = mOffset1
+
+                    // FAB 2
+                    mOffset2 = mFab0.y - mFab2.y
+                    mFab2.translationY = mOffset2
+
+                    // FAB 3
+                    mOffset3 = mFab0.y - mFab3.y
+                    mFab3.translationY = mOffset3
+
+                    mLayoutContainer.viewTreeObserver.removeOnPreDrawListener(this)
+
+                    return true
+                }
+            }
+        )
     }
 
     private fun configureExpandableFABs() {
@@ -130,14 +163,23 @@ class DiceActivity : BaseActivity(), View.OnClickListener {
         }
 
         // FAB 1
-        this.layout_fabs_fab_1.show()
+        //this.mFab1.show()
+        this.mFab1.translationY = - this.mOffset1
 
-//        AnimatorSet animatorSet = new AnimatorSet();
-//        animatorSet.playTogether(createExpandAnimator(fabAction1, offset1),
-//            createExpandAnimator(fabAction2, offset2),
-//            createExpandAnimator(fabAction3, offset3));
-//        animatorSet.start();
-//        animateFab();
+        // FAB 2
+        //this.mFab2.show()
+        this.mFab2.translationY = - this.mOffset2
+
+        // FAB 3
+        //this.mFab3.show()
+        this.mFab3.translationY = - this.mOffset3
+
+//        val animatorSet = AnimatorSet().apply {
+//            playTogether(createExpandAnimator(mFab1, mOffset1),
+//                         createExpandAnimator(mFab2, mOffset2),
+//                         createExpandAnimator(mFab3, mOffset3))
+//            start()
+//        }
     }
 
     private fun configureCollapseFABs() {
@@ -150,45 +192,30 @@ class DiceActivity : BaseActivity(), View.OnClickListener {
         }
 
         // FAB 1
-        this.layout_fabs_fab_1.hide()
+        //this.mFab1.hide()
+        this.mFab1.translationY = this.mOffset1
 
-//        AnimatorSet animatorSet = new AnimatorSet();
-//        animatorSet.playTogether(createCollapseAnimator(fabAction1, offset1),
-//            createCollapseAnimator(fabAction2, offset2),
-//            createCollapseAnimator(fabAction3, offset3));
-//        animatorSet.start();
-//        animateFab();
+        // FAB 2
+        //this.mFab2.hide()
+        this.mFab2.translationY = this.mOffset2
+
+        // FAB 3
+        //this.mFab3.hide()
+        this.mFab3.translationY = this.mOffset3
+
+//        val animatorSet = AnimatorSet().apply {
+//            playTogether(createCollapseAnimator(mFab1, mOffset1),
+//                         createCollapseAnimator(mFab2, mOffset2),
+//                         createCollapseAnimator(mFab3, mOffset3))
+//            start()
+//        }
     }
 
-//    private Animator createCollapseAnimator(View view, float offset) {
-//        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
-//            .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
-//    }
+    // -- ANIMATOR --
+
+//    private fun createCollapseAnimator(view: View, offset: Float) = ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0,offset)
+//                                                                                  .duration(resources.getInteger(R.integer.duration_animation))
 //
-//    private Animator createExpandAnimator(View view, float offset) {
-//        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 0)
-//            .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
-//    }
-//
-//    private void animateFab() {
-//        Drawable drawable = fab.getDrawable();
-//        if (drawable instanceof Animatable) {
-//            ((Animatable) drawable).start();
-//        }
-//    }
-//
-//    public void fabAction1(View view) {
-//        Log.d(TAG, "Action 1");
-//        Toast.makeText(this, "Go shopping!", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    public void fabAction2(View view) {
-//        Log.d(TAG, "Action 2");
-//        Toast.makeText(this, "Gimme money!", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    public void fabAction3(View view) {
-//        Log.d(TAG, "Action 3");
-//        Toast.makeText(this, "Turn it up!", Toast.LENGTH_SHORT).show();
-//    }
+//    private fun createExpandAnimator(view: View, offset: Float) = ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 0)
+//                                                                                .duration(resources.getInteger(R.integer.duration_animation))
 }
